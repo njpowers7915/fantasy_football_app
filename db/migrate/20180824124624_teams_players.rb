188b005b856a0204e8600eae8a788fa93cbd1639
njpowers7915 <njpowers7915@gmail.com>
2018-08-24 08:55:10 -0400

    Minor changes to migration files

--- db/migrate/20180824124624_teams_players.rb
+++ db/migrate/20180824124624_teams_players.rb
@@ -1,6 +1,8 @@
 class TeamsPlayers < ActiveRecord::Migration[5.2]
   def change
-    t.integer :team_id
-    t.integer :player_id
+    create_table :teams_players do |t|
+      t.integer :team_id
+      t.integer :player_id
+    end
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,71 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_08_24_124624) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "league_id"
+    t.integer "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "leagues", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "position_id"
+    t.integer "salary"
+    t.integer "points"
+    t.string "pro_team"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.integer "available_salary", default: 16
+    t.integer "user_id"
+    t.integer "points_total"
+    t.integer "league_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams_players", force: :cascade do |t|
+    t.integer "team_id"
+    t.integer "player_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.boolean "stat_admin", default: false
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
