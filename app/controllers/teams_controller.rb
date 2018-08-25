984e487fbd8e28019d0d4dbf1e18b2f9289859e6
njpowers7915 <njpowers7915@gmail.com>
2018-08-24 21:27:04 -0400

    Creating routes in Teams Controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,28 @@
 class TeamsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @team = @user.teams.build(user_id: params[:id])
+  end
+
+  def create
+    @team = Team.new(team_params)
+    @user = User.find(params[:user_id])
+    @team.user = @user
+    if @team.save
+      session[:team_id] = @team.id
+      redirect_to user_team_path(@user, @team)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    if Team.find_by_id(params[:id])
+      @team = Team.find_by_id(params[:id])
+      session[:team_id] = @team.id
+      @user = User.find_by_id(sessions[:user_id])
+      @players = @team.players
+    end
+  end
+
 end
