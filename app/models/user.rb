56149ce1643bd421ef88428f7d8335269cf61ecc
njpowers7915 <njpowers7915@gmail.com>
2018-08-24 20:09:31 -0400

    Validates User's name, password and email uniqueness and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
   has_many :teams
   has_many :comments, through: :teams
   has_many :leagues, through: :teams
