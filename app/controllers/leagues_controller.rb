680c265e65b3bc982c3d9afcf2a46c57b7ab5925
njpowers7915 <njpowers7915@gmail.com>
2018-08-26 08:46:16 -0400

    Admin can add users and view comments from League Show Page

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -14,6 +14,19 @@ class LeaguesController < ApplicationController
     else
       render 'new'
     end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
   end
 
   private
