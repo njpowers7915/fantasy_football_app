45b895d765788c85c92e8c25b93d5747c5a65674
njpowers7915 <njpowers7915@gmail.com>
2018-08-26 14:53:29 -0400

    Updating teams#update route

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -35,6 +35,27 @@ class TeamsController < ApplicationController
   def update
     @team = Team.find(params[:id])
     @user = @team.user
+    if !params["team"]["player"]["delete"]
+      if @team.update_attributes(team_params)
+        if params["team"]["player"] && !params["team"]["player"].empty?
+          player_array = params["team"]["player"].split(" --- ")
+          if player = Player.find_by(name: player_array[0], position: Position.find_by(name: player_array[1]))
+            player = Player.find_by(name: player_array[0], position: Position.find_by(name: player_array[1]))
+            if !@team.players.include?(player)
+              @team.players << player
+              redirect_to user_team_path(@user, @team)
+            else
+              flash[:notice] = "Player is already on this team"
+              redirect_to user_team_path(@user, @team)
+            end
+          else
+            flash[:notice] = "Player does not exist"
+            redirect_to user_team_path(@user, @team)
+          end
+        else
+          render 'edit'
+        end
+      end
     #NEED TO ELABORATE ON THIS ROUTE
   end
 
