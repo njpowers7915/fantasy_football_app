ae11ec0c8fd2c10ec92a52ea8038758b781887f9
njpowers7915 <njpowers7915@gmail.com>
2018-08-26 07:42:22 -0400

    League Controller new and create paths

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,2 +1,27 @@
 class LeaguesController < ApplicationController
+  def new
+    @league = League.new(league_params)
+  end
+
+  def create
+    @league = League.new(league_params)
+    @user = User.find(params[:user_id])
+    @league.users << @user
+    if @legaue.save
+      session[:league_id] = @league.id
+      redirect_to user_league_path(@user, @team)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def league_params
+    params.require(:league).permit(:name)
+  end
+
+  end
+
+
 end
