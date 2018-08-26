815f40e63aadfeacd11caca6c2706940337b661e
njpowers7915 <njpowers7915@gmail.com>
2018-08-26 19:33:55 -0400

    Updating comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new(comment_params)
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def comment_params
+    require(:comment).permit(:content)
+  end
+
 end
