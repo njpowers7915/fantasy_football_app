14ce8a50c4619a9a6c3c665cbb2904bb09df63ec
njpowers7915 <njpowers7915@gmail.com>
2018-08-26 14:27:06 -0400

    Find individual players by position method

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,28 @@ class Team < ApplicationRecord
   has_many :comments
   has_many :teams_players
   has_many :players, through: :teams_players
+
+  def get_hooker
+    self.players.where(position_id: 1)
+  end
+
+  def get_front_row
+    self.players.where(position_id: 2)
+  end
+
+  def get_second_row
+    self.players.where(position_id: 3)
+  end
+
+  def get_half
+    self.players.where(position_id: 4)
+  end
+
+  def self.get_centre
+    self.players.where(position_id: 5)
+  end
+
+  def outside_back
+    self.players.where(position_id: 6)
+  end
 end
