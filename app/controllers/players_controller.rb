0e837f63864fa27f561dcfd917213e1d6ca8078e
njpowers7915 <njpowers7915@gmail.com>
2018-08-28 10:56:04 -0400

    Updates to Players Controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,23 +1,31 @@
 class PlayersController < ApplicationController
+  before_action :set_player, only[:show]
 
   def show
-    @player = Player.find_by_id(params["id"])
+    render json: @player
   end
 
   def index
-    @players = Player.all
+    players = Player.all
+    render json: players
   end
 
-  def edit
-    @players = Player.find_by(params["id"])
-  end
+  #def edit
+  #  @players = Player.find_by(params["id"])
+  #end
 
-  def update
-    @player = Player.find_by_id(params["id"])
-    @team = Team.find_by_id(session["team_id"])
-    @team.delete_player(@player)
-    @team.save
-    redirect_to user_team_path(@team.user, @team)
+  #def update
+  #  @player = Player.find_by_id(params["id"])
+  #  @team = Team.find_by_id(session["team_id"])
+  #  @team.delete_player(@player)
+  #  @team.save
+  #  redirect_to user_team_path(@team.user, @team)
+  #end
+
+  private
+
+  def set_player
+    @player = Player.find(params[:id])
   end
 
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,4 @@
 class Player < ApplicationRecord
-  serialize :name, :position_id, :pro_team, :points, :touchdowns, :pass_yards, :rush_yards, :receptions, :rec_yards
-  
   belongs_to :position
   has_many :teams_players
   has_many :teams, through: :teams_players
