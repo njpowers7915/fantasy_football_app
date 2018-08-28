a8529cacbf8c8b26b3e25593b5ff2c71a39c389f
njpowers7915 <njpowers7915@gmail.com>
2018-08-28 09:12:09 -0400

    Removed User_Leagues Join table

--- app/models/user_league.rb
+++
@@ -1,6 +0,0 @@
-class UserLeague < ApplicationRecord
-  #validates :song_id, uniqueness: {scope: :playlist_id, message: "Song already exists in this playlist"}
-
-  belongs_to :user
-  belongs_to :league
-end
