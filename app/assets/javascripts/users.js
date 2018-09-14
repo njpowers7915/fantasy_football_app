2291523c54866d0d5735bcb071ec1cdf92ebfd6b
njpowers7915 <njpowers7915@gmail.com>
2018-09-13 20:37:34 -0400

    Button on user#show can show an index of all players

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,18 @@
-
+function getAllPlayers() {
+      //event.preventDefault()
+      $.getJSON('/players', function(data) {
+        var playerArray = []
+        data.data.forEach(function (player) {
+          var individualArray = [player.attributes.name, player.attributes.position.name,
+            player.attributes["pro-team"]]
+          playerArray.push(individualArray)
+        })
+        var statArray = playerArray.sort(function (a, b) {
+          return a[0] - b[0]
+        })
+        statArray.forEach(function (element) {
+          $("#allPlayers").append('<li>' + element[0] + '--' + element[1] + '--' + element[2] + '</li><br>')
+        })
+      })
+      return false
+    }
