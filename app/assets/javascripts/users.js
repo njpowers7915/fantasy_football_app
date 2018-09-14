df1bf40f34a115ce3c01d9000716c05103b40374
njpowers7915 <njpowers7915@gmail.com>
2018-09-13 21:00:59 -0400

    Moved JS user functions to user.js files

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,4 @@
+
 function getAllPlayers() {
       //event.preventDefault()
       $.getJSON('/players', function(data) {
@@ -16,3 +17,38 @@ function getAllPlayers() {
       })
       return false
     }
+
+function getStatLeaders() {
+  event.preventDefault()
+  $.getJSON('/players', function(data) {
+    var playerArray = []
+    data.data.forEach(function (player) {
+    var stat = $('option:selected')[0].value
+    var individualArray = [player.attributes.name, player.attributes.position.name,
+      player.attributes["pro-team"], player.attributes[stat]]
+    playerArray.push(individualArray)
+    })
+  var statArray = playerArray.sort(function (a, b) {
+    return b[3] - a[3]
+    }).slice(0, 5)
+  var stat_name = { stat_name: $('option:selected')[0].innerHTML }
+  var templateArray = []
+  statArray.forEach(function (player) {
+    var individualStats = {
+      name: player[0],
+      position: player[1],
+      pro_team: player[2],
+      stat: player[3]
+      //stat_name: $('option:selected')[0].innerHTML,
+    }
+    templateArray.push(individualStats)
+    })
+    var template_header = Handlebars.compile(document.getElementById("stat_header_template").innerHTML)
+    var template_body = Handlebars.compile(document.getElementById("stat_body_template").innerHTML)
+    var header_result = template_header(stat_name)
+    var body_result = template_body(templateArray)
+    document.getElementById("statLeadersHeader").innerHTML = header_result
+    document.getElementById("statLeadersBody").innerHTML = body_result
+  })
+  return false
+}
